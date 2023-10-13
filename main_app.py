ae5663ada3ee5a7ff78600518a991e48ba8f23b8
JinukHong <45095330+JinukHong@users.noreply.github.com>
2023-10-13 18:17:00 +0900

    footer

--- main_app.py
+++ main_app.py
@@ -28,5 +28,36 @@ def main():
             st.session_state.current_question = len(questions) - 1
     else:
        show_survey()
+    
+    hide_footer_style = """
+<style>
+#MainMenu {visibility: hidden;} 
+div.block-container {padding-top:1rem;}
+div.block-container {padding-bottom:3rem;}
+}
+</style>
+"""
+    st.markdown(hide_footer_style, unsafe_allow_html=True)
+
+    footer_setup = '''
+<script>
+// To break out of iframe and access the parent window
+const streamlitDoc = window.parent.document;
+
+// Make the replacement
+document.addEventListener("DOMContentLoaded", function(event){
+    const footer = streamlitDoc.getElementsByTagName("footer")[0];
+    footer.innerHTML = `
+        Provided by 
+        <a target="_blank" class="css-z3au9t egzxvld2">Team MiZi(미지)</a>
+        <img src="https://i.namu.wiki/i/RMFfYwm6uMpMAbnzBf9ZKX2mM3ro6TzG-FSCPOnyxT5pZQdUc0Ftp6pq3wGuHcBz74ly-Nt7JwkypXDNS3kqV9yfXVoeziDMJxlWIwsH816HwkzN4kGTuCElz4iMUvg6Ckdjy91lGUZ-UDcwghpR0g.webp" alt="DGB" height="30">
+        
+    `;
+});
+</script>
+'''
+
+    st.components.v1.html(footer_setup)
+    
 if __name__ == "__main__":
     main()
